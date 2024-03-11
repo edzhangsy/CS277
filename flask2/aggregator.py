fb29666f0905592d432ca5b0bec7b1f262c58348
jrodr033 <jrodr033@ucr.edu>
2024-03-11 16:40:48 -0700

    update

--- flask2/aggregator.py
+++ flask2/aggregator.py
@@ -113,7 +113,7 @@ def aggregate():
         print(f"iterations: {iterations}")
         # Send the results
         if iterations > 0:
-            iterations -= iterations
+            iterations -= 1
             for i in range(len(clients)):
                 for j in range(4):
                     file_path = "../mnist_model/weights/torch_weights"+str(i)+".json"

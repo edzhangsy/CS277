12b7ed40f5ee53426a7910d2a37e51f2e69d6949
Joseph Rodriguez <Joseph_Rodriguez@MacBook-Pro.local>
2024-03-10 22:13:17 -0700

    up

--- flask2/switch.py
+++ flask2/switch.py
@@ -87,6 +87,6 @@ def add():
             file_path = f"../mnist_model/weights/{address2}_torch_weights"+str(i)+".json"
             with open(file_path, "rb") as f:
                 files = {"file" : (file_path, f.read())}
-                request.post(f"http://{address}:5000/", files=files)
+                requests.post(f"http://{address}:5000/", files=files)
 
     return ""

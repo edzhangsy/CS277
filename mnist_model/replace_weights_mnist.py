28476c697e202331bbf8ef2570152f0b46555bdc
Kevin <stu024@ucr.edu>
2024-03-20 19:50:35 -0600

    updates mnist.py files to take in arguments for client

--- mnist_model/replace_weights_mnist.py
+++ mnist_model/replace_weights_mnist.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda
 import torch
@@ -12,143 +12,179 @@ import ast
 from torchvision import transforms
 from PIL import Image
 import numpy as np
+import sys
 
-### Grabbing MNIST data with torchvision datasets
-training_data = datasets.MNIST(
-   root = 'data',
-   train = True,
-   download = True,
-   transform = ToTensor()
-)
-test_data = datasets.MNIST(
-   root = 'data',
-   train = False,
-   download = True,
-   transform = ToTensor()
-)
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 3:
+        print("Usage: python replace_weights_mnist.py numClients clientIndex")
+        sys.exit(1)
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-#print(device)
-#print(torch.cuda.device_count())
-#print(torch.cuda.current_device())
-#print(torch.cuda.device(0))
-#print(torch.cuda.get_device_name(0))
+    # Retrieve the arguments
+    numClients = int(sys.argv[1])
+    clientIndex = int(sys.argv[2])
 
-training_data[0][0].shape
-training_data[0][0].squeeze().shape
-training_data = list(map(list, training_data))
-test_data = list(map(list, test_data))
+    # Print the arguments
+    print("Argument 1:", numClients)
+    print("Argument 2:", clientIndex)
+        
+    ### Grabbing MNIST data with torchvision datasets
+    training_data = datasets.MNIST(
+    root = 'data',
+    train = True,
+    download = True,
+    transform = ToTensor()
+    )
+    test_data = datasets.MNIST(
+    root = 'data',
+    train = False,
+    download = True,
+    transform = ToTensor()
+    )
 
-for count in range(len(training_data)):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    #print(device)
+    #print(torch.cuda.device_count())
+    #print(torch.cuda.current_device())
+    #print(torch.cuda.device(0))
+    #print(torch.cuda.get_device_name(0))
 
-    # Convert the PyTorch tensor to a PIL Image
-    pil_image = transforms.ToPILImage()(training_data[count][0])
+    training_data[0][0].shape
+    training_data[0][0].squeeze().shape
+    training_data = list(map(list, training_data))
+    test_data = list(map(list, test_data))
 
-    # Resize the image to 16x16
-    resized_image = pil_image.resize((12, 12))
+    for count in range(len(training_data)):
 
-    # Convert the resized image back to a PyTorch tensor
-    training_data[count][0] = transforms.ToTensor()(resized_image)
+        # Convert the PyTorch tensor to a PIL Image
+        pil_image = transforms.ToPILImage()(training_data[count][0])
 
-for count in range(len(test_data)):
-    
-    # Convert the PyTorch tensor to a PIL Image
-    pil_image = transforms.ToPILImage()(test_data[count][0])
+        # Resize the image to 16x16
+        resized_image = pil_image.resize((12, 12))
 
-    # Resize the image to 16x16
-    resized_image = pil_image.resize((12, 12))
+        # Convert the resized image back to a PyTorch tensor
+        training_data[count][0] = transforms.ToTensor()(resized_image)
 
-    # Convert the resized image back to a PyTorch tensor
-    test_data[count][0] = transforms.ToTensor()(resized_image)
-    
-train_dataloader = DataLoader (training_data, batch_size = 64)
-test_dataloader = DataLoader(test_data, batch_size = 64)
+    for count in range(len(test_data)):
+        
+        # Convert the PyTorch tensor to a PIL Image
+        pil_image = transforms.ToPILImage()(test_data[count][0])
 
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(12*12, 32),
-            nn.ReLU(),
-            nn.Linear(32, 10)
-        )
+        # Resize the image to 16x16
+        resized_image = pil_image.resize((12, 12))
 
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+        # Convert the resized image back to a PyTorch tensor
+        test_data[count][0] = transforms.ToTensor()(resized_image)
+        
+    # Define the fraction you want to use (0, 1, 2, or 3)
+    chosen_fraction = clientIndex  # Change this to select a different fraction (0-indexed)
 
-model = NeuralNetwork().to(device)
-#model = NeuralNetwork().to('cpu')
+    # Assuming len(training_data) and len(test_data) are known
+    fraction_train_length = len(training_data) // numClients
+    fraction_test_length = len(test_data) // numClients
 
-state_dict = model.state_dict()
+    start_index_train = chosen_fraction * fraction_train_length
+    end_index_train = (chosen_fraction + 1) * fraction_train_length
 
-keys_list = ['linear_relu_stack.0.weight', 'linear_relu_stack.0.bias', 'linear_relu_stack.2.weight', 'linear_relu_stack.2.bias']
+    start_index_test = chosen_fraction * fraction_test_length
+    end_index_test = (chosen_fraction + 1) * fraction_test_length
 
-for count in range(len(keys_list)):
-    with open('../mnist_model/weights/torch_weights'+str(count)+'.json', 'r') as json_file:
-        state_dict[keys_list[count]] = torch.Tensor(ast.literal_eval(json_file.read()))
+    # Create subsets for the chosen fraction of the datasets
+    train_subset = Subset(training_data, range(start_index_train, end_index_train))
+    test_subset = Subset(test_data, range(start_index_test, end_index_test))
 
-#for count in range(len(keys_list)):
-#    with open('./aggregate/ckks_weights'+str(count)+'.json', 'r') as json_file:
-#       state_dict[keys_list[count]] = torch.Tensor(ast.literal_eval(json_file.read()))
+    # Create data loaders for the subsets
+    train_dataloader = DataLoader(train_subset, batch_size=64)
+    test_dataloader = DataLoader(test_subset, batch_size=64)
+        
+    # train_dataloader = DataLoader (training_data, batch_size = 64)
+    # test_dataloader = DataLoader(test_data, batch_size = 64)
 
-model.load_state_dict(state_dict)
+    class NeuralNetwork(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.flatten = nn.Flatten()
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(12*12, 32),
+                nn.ReLU(),
+                nn.Linear(32, 10)
+            )
 
-lr = 1e-3
-bs = 64
-epochs = 5
+        def forward(self, x):
+            x = self.flatten(x)
+            logits = self.linear_relu_stack(x)
+            return logits
 
-loss_fn = nn.CrossEntropyLoss()
+    model = NeuralNetwork().to(device)
+    #model = NeuralNetwork().to('cpu')
 
-optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+    state_dict = model.state_dict()
 
-def train_data(model):
-    for xb, yb in train_dataloader:
-        xb = xb.clone().detach().to(device)
-        yb = yb.clone().detach().to(device)
-        preds = model(xb)
-        loss = loss_fn(preds, yb)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    loss = loss.item()
-    print(f"Train loss: {loss:>7f}")
+    keys_list = ['linear_relu_stack.0.weight', 'linear_relu_stack.0.bias', 'linear_relu_stack.2.weight', 'linear_relu_stack.2.bias']
 
-def test_data(model):
-    num_batches = len(test_dataloader)
-    size = len(test_dataloader.dataset)
-    test_loss, corrects = 0, 0
+    for count in range(len(keys_list)):
+        with open('../mnist_model/weights/torch_weights'+str(count)+'.json', 'r') as json_file:
+            state_dict[keys_list[count]] = torch.Tensor(ast.literal_eval(json_file.read()))
 
-    with torch.no_grad():
-        for xb, yb in test_dataloader:
+    #for count in range(len(keys_list)):
+    #    with open('./aggregate/ckks_weights'+str(count)+'.json', 'r') as json_file:
+    #       state_dict[keys_list[count]] = torch.Tensor(ast.literal_eval(json_file.read()))
+
+    model.load_state_dict(state_dict)
+
+    lr = 1e-3
+    bs = 64
+    epochs = 5
+
+    loss_fn = nn.CrossEntropyLoss()
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+
+    def train_data(model):
+        for xb, yb in train_dataloader:
             xb = xb.clone().detach().to(device)
             yb = yb.clone().detach().to(device)
             preds = model(xb)
-            test_loss += loss_fn(preds, yb).item()
-            corrects += (preds.argmax(1) == yb).type(torch.float).sum().item()
+            loss = loss_fn(preds, yb)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        loss = loss.item()
+        print(f"Train loss: {loss:>7f}")
 
-    test_loss /= num_batches
-    # test_loss = lo
-    corrects /= size
-    print(f"Test loss: \n Accuracy: {(100*corrects):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    def test_data(model):
+        num_batches = len(test_dataloader)
+        size = len(test_dataloader.dataset)
+        test_loss, corrects = 0, 0
 
-for t in range(4):
-    train_data(model)
-    test_data(model)
+        with torch.no_grad():
+            for xb, yb in test_dataloader:
+                xb = xb.clone().detach().to(device)
+                yb = yb.clone().detach().to(device)
+                preds = model(xb)
+                test_loss += loss_fn(preds, yb).item()
+                corrects += (preds.argmax(1) == yb).type(torch.float).sum().item()
 
-#torch.save(model.state_dict(), './state/model.pth')
+        test_loss /= num_batches
+        # test_loss = lo
+        corrects /= size
+        print(f"Test loss: \n Accuracy: {(100*corrects):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-class EncodeTensor(JSONEncoder,Dataset):
-    def default(self, obj):
-        if isinstance(obj, torch.Tensor):
-            return obj.cpu().detach().numpy().tolist()
-        return super(EncodeTensor, self).default(obj)
+    for t in range(4):
+        train_data(model)
+        test_data(model)
 
-#with open('torch_weights.json', 'w') as json_file:
-#    json.dump(model.state_dict(), json_file,cls=EncodeTensor)
+    #torch.save(model.state_dict(), './state/model.pth')
 
-for count, param_tensor in enumerate(model.state_dict()):
-    with open('../mnist_model/weights/torch_weights'+str(count)+'.json', 'w') as json_file:
-        json.dump(model.state_dict()[param_tensor], json_file,cls=EncodeTensor)
+    class EncodeTensor(JSONEncoder,Dataset):
+        def default(self, obj):
+            if isinstance(obj, torch.Tensor):
+                return obj.cpu().detach().numpy().tolist()
+            return super(EncodeTensor, self).default(obj)
+
+    #with open('torch_weights.json', 'w') as json_file:
+    #    json.dump(model.state_dict(), json_file,cls=EncodeTensor)
+
+    for count, param_tensor in enumerate(model.state_dict()):
+        with open('../mnist_model/weights/torch_weights'+str(count)+'.json', 'w') as json_file:
+            json.dump(model.state_dict()[param_tensor], json_file,cls=EncodeTensor)
